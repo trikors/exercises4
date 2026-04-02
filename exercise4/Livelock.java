df002c4d4a1eb20ef14ace01384c24d6cb4229c3
trikors <kotuzhansky@gmail.com>
2026-04-02 17:16:07 +0300

    Add files via upload

--- exercise4/Livelock.java
+++ exercise4/Livelock.java
@@ -1,19 +1,24 @@
 package exercise4;
 
-class Spouse{
-    private String name;
+class Spouse {
+    private final String name;
     private boolean isHungry = true;
 
-    public Spouse(String name){ this.name = name; }
+    public Spouse(String name) {
+        this.name = name;
+    }
 
-    public void eat(Spoon spoon, Spouse otherSpouse){
-        while(isHungry){
-            if(spoon.getOwner() != this){
-                try{ Thread.sleep(10); } catch (InterruptedException e){}
+    public void eat(Spoon spoon, Spouse otherSpouse) {
+        while (isHungry) {
+            if (spoon.getOwner() != this) {
+                try {
+                    Thread.sleep(10);
+                } catch (InterruptedException e) {
+                }
                 continue;
             }
 
-            if(otherSpouse.isHungry){
+            if (otherSpouse.isHungry) {
                 System.out.println(name + " you eat first!");
                 spoon.setOwner(otherSpouse);
                 continue;
@@ -27,12 +32,24 @@ class Spouse{
     }
 }
 
-class Spoon{
+class Spoon {
     private Spouse owner;
-    public Spoon(Spouse s){ owner = s; }
-    public synchronized Spouse getOwner() { return owner; }
-    public synchronized void setOwner(Spouse s){ owner = s; }
-    public synchronized void use() { System.out.println("Eating..."); }
+
+    public Spoon(Spouse s) {
+        owner = s;
+    }
+
+    public synchronized Spouse getOwner() {
+        return owner;
+    }
+
+    public synchronized void setOwner(Spouse s) {
+        owner = s;
+    }
+
+    public synchronized void use() {
+        System.out.println("Eating...");
+    }
 }
 
 public class Livelock {
@@ -41,8 +58,12 @@ public class Livelock {
         final Spouse wife = new Spouse("Natasha");
         final Spoon spoon = new Spoon(husband);
 
-        Runnable task1 = () -> { husband.eat(spoon, wife); };
-        Runnable task2 = () -> { wife.eat(spoon, husband); };
+        Runnable task1 = () -> {
+            husband.eat(spoon, wife);
+        };
+        Runnable task2 = () -> {
+            wife.eat(spoon, husband);
+        };
 
         new Thread(task1).start();
         new Thread(task2).start();
