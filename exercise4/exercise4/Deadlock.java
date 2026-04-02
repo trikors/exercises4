da4bd1c875e425036bfe2018dcfa08418597494a
trikors <kotuzhansky@gmail.com>
2026-04-02 17:25:17 +0300

    Add files via upload

---
+++ exercise4/exercise4/Deadlock.java
@@ -0,0 +1,42 @@
+package exercise4;
+
+public class Deadlock {
+    private static final Object object1 = new Object();
+    private static final Object object2 = new Object();
+
+    public static void main(String[] args) {
+        Runnable task1 = () -> {
+            synchronized (object1) {
+                System.out.println("Thread 1 on object1");
+                try {
+                    Thread.sleep(200);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                synchronized (object2) {
+                    System.out.println("Thread 1 on object2");
+                }
+            }
+        };
+
+        Runnable task2 = () -> {
+            synchronized (object2) {
+                System.out.println("Thread 2 on object2");
+                try {
+                    Thread.sleep(200);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                synchronized (object1) {
+                    System.out.println("Thread 2 on object 1");
+                }
+            }
+        };
+
+        Thread thread1 = new Thread(task1);
+        Thread thread2 = new Thread(task2);
+
+        thread1.start();
+        thread2.start();
+    }
+}
---
+++ exercise4/exercise4/Livelock.java
@@ -0,0 +1,71 @@
+package exercise4;
+
+class Spouse {
+    private final String name;
+    private boolean isHungry = true;
+
+    public Spouse(String name) {
+        this.name = name;
+    }
+
+    public void eat(Spoon spoon, Spouse otherSpouse) {
+        while (isHungry) {
+            if (spoon.getOwner() != this) {
+                try {
+                    Thread.sleep(10);
+                } catch (InterruptedException e) {
+                }
+                continue;
+            }
+
+            if (otherSpouse.isHungry) {
+                System.out.println(name + " you eat first!");
+                spoon.setOwner(otherSpouse);
+                continue;
+            }
+
+            spoon.use();
+            isHungry = false;
+            System.out.println(name + ": I am full!");
+            spoon.setOwner(otherSpouse);
+        }
+    }
+}
+
+class Spoon {
+    private Spouse owner;
+
+    public Spoon(Spouse s) {
+        owner = s;
+    }
+
+    public synchronized Spouse getOwner() {
+        return owner;
+    }
+
+    public synchronized void setOwner(Spouse s) {
+        owner = s;
+    }
+
+    public synchronized void use() {
+        System.out.println("Eating...");
+    }
+}
+
+public class Livelock {
+    public static void main(String[] args) {
+        final Spouse husband = new Spouse("Ivan");
+        final Spouse wife = new Spouse("Natasha");
+        final Spoon spoon = new Spoon(husband);
+
+        Runnable task1 = () -> {
+            husband.eat(spoon, wife);
+        };
+        Runnable task2 = () -> {
+            wife.eat(spoon, husband);
+        };
+
+        new Thread(task1).start();
+        new Thread(task2).start();
+    }
+}
